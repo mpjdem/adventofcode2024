d3403334b70dfe97066918f7157d93fe67c8092a
Maarten Demeyer <mpjdem@gmail.com>
2024-12-02 10:50:04 +0000

    Because we love one-liners

--- aoc24_day1.R
+++ aoc24_day1.R
@@ -17,10 +17,6 @@ solution_1 <- dt[, sum(abs(sort(V1) - sort(V2)))]
 check_solution(1, 1, solution_1)
 
 ## -- PART 2 --
-solution_2 <- merge(
-  dt[, .(V1)], 
-  dt[, .N, by=.(V2)], 
-  by.x="V1", by.y="V2", all=FALSE
-)[, sum(V1 * N)]
+solution_2 <- merge(dt, dt[, .N, by="V2"], by.x="V1", by.y="V2")[, sum(V1 * N)]
 
 check_solution(1, 2, solution_2)
